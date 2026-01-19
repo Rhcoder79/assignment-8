29275f4139b1e03a8fdbac4ffd17f01460b7b59a
Rhcoder79 <robiulhasan7972171@gmail.com>
2026-01-19 20:50:55 +0600

    hero section complete

--- my-react-app/src/Pages/Home/Home.jsx
+++ my-react-app/src/Pages/Home/Home.jsx
@@ -1,23 +1,69 @@
 import React from 'react';
 import play from '../../icons/play-store.png'
 import app from '../../icons/app-store.png'
+import hero from '../../assets/hero.png'
+import download from "../../assets/icon-downloads.png"
+import ratings from "../../assets/icon-ratings.png"
 const Home = () => {
     return (
-        <section className='p-10 bg-[#f5f5f5] text-black '>
-            <div className='max-w-9/10 mx-auto  '>
-                <div className='flex flex-col  items-center text-center'>
+        <section className=' bg-[#f5f5f5] text-black '>
+            <div className='md:p-10 p-5 max-w-9/10 mx-auto  '>
+                <div className='flex flex-col gap-2  items-center text-center'>
                 <div><h1 className='text-5xl font-bold'>We Build <br />
                 <span className='text-blue-800'>Productive</span> Apps</h1></div>
                 <div >
-                    <p className='opacity-90 max-w-[60%] mx-auto '>At <span className='font-bold text-blue-800 '>HERO.IO, </span>
+                    <p className='sm:opacity-90 sm:max-w-[60%] sm:mx-auto text-xl sm:text-center text-left '>At 
+                        <span className='font-bold text-blue-800 '> HERO.IO, </span>
                       we craft innovative apps designed to make everyday life simpler, smarter, and more exciting.
                     Our goal is to turn your ideas into digital experiences that truly make an impact.</p>
                 </div>
-                <div>
-                    <button><img src={play} alt="image not found" /> Google Play</button>
-                    <button><img src={app} alt="image not found" /> App Store</button>
+                <div className='flex gap-3 flex-wrap justify-center '>
+                    <a href="https://play.google.com/store" target='_blank'><button className='btn bg-white text-black font-bold border-none'>
+                        <img src={play} alt="image not found" /> Google Play</button></a>
+                    <a href="https://www.apple.com/app-store/" target='_blank'>
+                        <button className='btn bg-white text-black font-bold border-none'>
+                        <img src={app} alt="image not found" /> App Store</button>
+                    </a>
                 </div>
             </div>
+            </div>
+             <div className='  mt-5 bg-red-600 overflow-hidden'>
+               <div className='flex justify-center'> <img  src={hero} alt="image not found"  /></div>
+                <div className='bg-blue-600 text-white text-center p-5 md:p-10'>
+                    <h1 className='font-bold text-3xl'>Trusted by Millions, Built for You </h1> <br />
+                    <div className='flex justify-evenly  max-sm:flex-wrap '>
+                        <div className='flex  gap-4 items-center'>
+                            <div className='flex flex-col gap-3'>
+                                <p className='font-semibold'>Total Downloads</p>
+                            <h1 className='text-3xl font-bold'>29.6M</h1>
+                             <p>21% more than last month</p>
+                            </div>
+                            <div>
+                                <img src={download} alt="not found" className='btn btn-soft' />
+                            </div>
+                            </div>
+                        <div className='flex  gap-4 items-center'>
+                           <div className='flex flex-col gap-3 '>
+                             <p className='font-semibold'>Total Reviews</p>
+                            <h1 className='text-3xl font-bold' >906K</h1>
+                             <p>46% more than last month</p>
+                           </div>
+                           <div>
+                            <img src={ratings} alt="" />
+                           </div>
+                        </div>
+                        <div className='flex gap-4 items-center'>
+                            <div className='flex flex-col  gap-3'>
+                                <p className='font-semibold'>Active Apps</p>
+                            <h1 className='text-3xl font-bold'>132+</h1>
+                             <p>31% more with Launch</p>
+                            </div>
+                            <div>
+                                <img src={play} alt="" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     );
